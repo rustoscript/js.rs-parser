e799df660c6f1154fbc53bc13b3b44aea3eabdd1
Saghm Rossi <saghmrossi@gmail.com>
2015-10-11 13:39:57 -0400

    add rudimentary `Display` for expressions (no grouping yet)

--- src/ast.rs
+++ src/ast.rs
@@ -1,3 +1,6 @@
+use std::fmt::{Display, Error, Formatter};
+
+
 #[derive(Debug, PartialEq, Eq)]
 pub enum BinOp {
     Star,
@@ -5,9 +8,29 @@ pub enum BinOp {
     Minus,
 }
 
+impl Display for BinOp {
+    fn fmt(&self, fmt: &mut Formatter) -> Result<(), Error> {
+        match self {
+            &BinOp::Star => write!(fmt, "*"),
+            &BinOp::Plus => write!(fmt, "+"),
+            &BinOp::Minus => write!(fmt, "-"),
+        }
+    }
+}
+
 #[derive(Debug)]
 pub enum Exp {
     Int(i64),
     Float(f64),
     BinExp(Box<Exp>, BinOp, Box<Exp>),
+}
+
+impl Display for Exp {
+    fn fmt(&self, fmt: &mut Formatter) -> Result<(), Error> {
+        match self {
+            &Exp::Int(i) => write!(fmt, "{}", i),
+            &Exp::Float(f) => write!(fmt, "{}", f),
+            &Exp::BinExp(ref e1, ref o, ref e2) => write!(fmt, "{} {} {}", e1, o, e2),
+        }
+    }
 }
