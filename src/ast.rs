aae0c926c1e595661c322e8782a2a4e9b9d1ebe2
Saghm Rossi <saghmrossi@gmail.com>
2015-10-11 14:18:32 -0400

    added predecedence to facilitate proper printing

--- src/ast.rs
+++ src/ast.rs
@@ -1,11 +1,26 @@
 use std::fmt::{Display, Error, Formatter};
 
-
 #[derive(Debug, PartialEq, Eq)]
 pub enum BinOp {
     Star,
     Plus,
     Minus,
+}
+
+#[derive(PartialEq, Eq, PartialOrd, Ord)]
+pub enum Precedence {
+    Const = 100,
+    Mult = 60,
+    Add = 50,
+}
+
+impl BinOp {
+    pub fn precedence(&self) -> Precedence {
+        match self {
+            &BinOp::Star => Precedence::Mult,
+            &BinOp::Plus | &BinOp::Minus => Precedence::Add,
+        }
+    }
 }
 
 impl Display for BinOp {
@@ -25,12 +40,37 @@ pub enum Exp {
     BinExp(Box<Exp>, BinOp, Box<Exp>),
 }
 
+impl Exp {
+    pub fn precedence(&self) -> Precedence {
+        match self {
+            &Exp::Int(_) | &Exp::Float(_) => Precedence::Const,
+            &Exp::BinExp(_, ref o, _) => o.precedence(),
+        }
+    }
+}
+
 impl Display for Exp {
     fn fmt(&self, fmt: &mut Formatter) -> Result<(), Error> {
         match self {
             &Exp::Int(i) => write!(fmt, "{}", i),
             &Exp::Float(f) => write!(fmt, "{}", f),
-            &Exp::BinExp(ref e1, ref o, ref e2) => write!(fmt, "{} {} {}", e1, o, e2),
+            &Exp::BinExp(ref e1, ref o, ref e2) => {
+                let prec = self.precedence();
+
+                let left = if prec > e1.precedence() {
+                    format!("({})", e1)
+                } else {
+                    format!("{}", e1)
+                };
+
+                let right = if prec > e2.precedence() {
+                    format!("({})", e2)
+                } else {
+                    format!("{}", e2)
+                };
+
+                write!(fmt, "{} {} {}", left, o, right)
+            }
         }
     }
 }
