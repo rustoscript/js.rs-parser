5e19ce2c4c7f24fca76380bca38fd82498377daf
Saghm Rossi <saghmrossi@gmail.com>
2015-11-02 12:38:15 -0500

    made binary more usable

--- src/bin/parse.rs
+++ src/bin/parse.rs
@@ -1,11 +1,30 @@
 extern crate jsrs_parser;
 
 use jsrs_parser::lalr::parse_Stmt;
-use std::io;
+use std::io::{self, Write};
 
 fn main() {
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
+    let stdin = io::stdin();
+    let mut stdout = io::stdout();
 
-    println!("{:?}", parse_Stmt(&input).unwrap());
+    loop {
+        print!(">> ");
+        stdout.flush().unwrap();
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
+
+        if &input == ".exit\n" {
+            return;
+        }
+
+        let stmt = match parse_Stmt(&input) {
+            Ok(s) => s,
+            _ => {
+                println!("Error: failed to parse");
+                continue
+            }
+        };
+
+        println!("=> {:?}", stmt);
+    }
 }
