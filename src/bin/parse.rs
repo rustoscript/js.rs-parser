70236dbdc98e5f13d68078930adff7b086aac1c1
Saghm Rossi <saghmrossi@gmail.com>
2015-11-11 22:04:17 -0500

    use stderr for the error messages

--- src/bin/parse.rs
+++ src/bin/parse.rs
@@ -6,6 +6,7 @@ use std::io::{self, Write};
 fn main() {
     let stdin = io::stdin();
     let mut stdout = io::stdout();
+    let mut stderr = io::stderr();
 
     loop {
         print!(">> ");
@@ -20,7 +21,7 @@ fn main() {
         let stmt = match parse_Stmt(&input) {
             Ok(s) => s,
             _ => {
-                println!("Error: failed to parse");
+                writeln!(stderr, "Error: failed to parse").unwrap();
                 continue
             }
         };
