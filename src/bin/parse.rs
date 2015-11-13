b32bd345a248e89c257517c681a71ed9c52bb93c
Saghm Rossi <saghmrossi@gmail.com>
2015-11-12 23:03:04 -0500

    use rustlyine in binary for better io

--- src/bin/parse.rs
+++ src/bin/parse.rs
@@ -1,31 +1,41 @@
 extern crate jsrs_parser;
+extern crate rustyline;
 
 use jsrs_parser::lalr::parse_Stmt;
+use rustyline::Editor;
+use std::fs::metadata;
 use std::io::{self, Write};
 
 fn main() {
-    let stdin = io::stdin();
-    let mut stdout = io::stdout();
-    let mut stderr = io::stderr();
+    let mut err = io::stderr();
+    let mut rl = Editor::new();
+
+    if let Ok(_) = metadata(".history") {
+        if let Err(_) = rl.load_history(".history") {
+            writeln!(err, "Error: unable to load history on startup").unwrap();
+        }
+    }
 
     loop {
-        print!(">> ");
-        stdout.flush().unwrap();
-        let mut input = String::new();
-        stdin.read_line(&mut input).unwrap();
+        let input = rl.readline(">> ").unwrap();
 
-        if &input == ".exit\n" {
-            return;
+        if input.eq(".exit") {
+            if let Err(_) = rl.save_history(".history") {
+                writeln!(err, "Error: unable to save history on exit").unwrap();
+            }
+
+            break;
         }
 
+        rl.add_history_entry(&input);
         let stmt = match parse_Stmt(&input) {
             Ok(s) => s,
             _ => {
-                writeln!(stderr, "Error: failed to parse").unwrap();
-                continue
+                writeln!(err, "Error: unable to parse statement").unwrap();
+                continue;
             }
         };
 
-        println!("=> {:?}", stmt);
+        println!("{:?}", stmt);
     }
 }
